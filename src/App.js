7da23f9c3ab52be3b3a40839e6cc6e7478eddb09
by-yz <bylalaitabbas26@gmail.com>
2024-05-30 19:39:42 +0200

    ok

--- src/App.js
+++ src/App.js
@@ -4,10 +4,30 @@ import Footerpage from './component/footer.jsx';
 
 function App() {
   return (
+<<<<<<< HEAD
     <div>
       <p>test hahaha</p>
       <Footerpage/>
     
+=======
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          lets go the codeok
+
+          dddd
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn Reactssssssssss
+        </a>
+      </header>
+>>>>>>> 393adda813dc515e2dbd42bf8de608491ef41677
     </div>
   );
 }
---
+++ src/component/footer.jsx
@@ -0,0 +1,15 @@
+const footerpage = () => {
+    return (
+        <div className="text-center" >
+            <p id="footer1" >Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus magni tempora itaque voluptatem iste placeat eum neque doloribus! Voluptas explicabo aliquam illum? Minus unde iusto at aliquid eveniet numquam hic.</p>
+
+            <div>
+                <p className="border">button</p>
+            </div>
+
+        </div >
+    )
+}
+
+
+export default footerpage;
